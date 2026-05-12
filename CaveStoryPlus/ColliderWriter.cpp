b47949c56905b9bda3e745fbf493d47d349372dd
bramderaeve <bram.deraeve@student.howest.be>
2026-05-12 15:43:13 +0200

    tweak collider

--- CaveStoryPlus/ColliderWriter.cpp
+++ CaveStoryPlus/ColliderWriter.cpp
@@ -1,24 +1,31 @@
-#include "pch.h"
-#include "PolygonCollider.h"
 #include "ColliderWriter.h"
+#include "PolygonCollider.h"
+#include "pch.h"
 
+#include <filesystem>
 #include <iostream>
 
-ColliderWriter::ColliderWriter(const std::string& colliderFilePath)
+ColliderWriter::ColliderWriter(const std::string &colliderFilePath)
     : m_Stream(colliderFilePath), m_FilePath(colliderFilePath)
 {
 }
 
 ColliderWriter::~ColliderWriter()
 {
-    std::cout << "Wrote colliders to: " << m_FilePath << std::endl;
+    std::error_code err{};
+    std::filesystem::path fullPath{std::filesystem::canonical(std::filesystem::current_path() / m_FilePath, err)};
+    if (err)
+    {
+        fullPath = m_FilePath;
+    }
+    std::cout << "Wrote colliders to: " << fullPath.string() << std::endl;
 }
 
-void ColliderWriter::Serialize(const PolygonCollider& collider)
+void ColliderWriter::Serialize(const PolygonCollider &collider)
 {
     m_Stream << "[PolygonCollider]\n";
 
-    const std::vector<Vector2f>& colliders{ collider.GetPolygon() };
+    const std::vector<Vector2f> &colliders{collider.GetPolygon()};
     for (int i{}; i < colliders.size(); ++i)
     {
         m_Stream << colliders[i].x << ";" << colliders[i].y << '\n';
